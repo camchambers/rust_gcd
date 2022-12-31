e5530fb20ebbbbf67b54fc1e6331f36177bc65bc
Cam <cam_chambers@outlook.com>
2022-12-31 08:43:34 -0500

    Working GCD example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Greatest Common Divisor - Euclid's Algorithm");
+    let first_number = 128;
+    let second_number = 60;
+    println!(
+        "GCD({}, {}) = {}",
+        first_number,
+        second_number,
+        gcd(first_number, second_number)
+    );
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
